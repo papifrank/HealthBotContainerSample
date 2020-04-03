27cf9eee7cfc4ca31f9d43eb8c1b21cd384cc55d
papifrank <github@rebootcop.com>
2020-04-02 22:38:07 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -109,7 +109,7 @@ function initBotConversation() {
                             // Use the following activity to proactively invoke a bot scenario
                             
                             triggeredScenario: {
-                                trigger: "covid19_touchstone"
+                                trigger: "covid19_assessment"
                                 
                             }
                             
